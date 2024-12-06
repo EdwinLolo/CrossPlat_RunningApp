b0adccc4d67bcb50bad77a91c65bebea35d0daf6
Acipzz <moses.alexander@student.umn.ac.id>
2024-12-06 21:46:49 +0700

    ngilangin warning lagi

--- RunningApp/Components/ProfileOptions.js
+++ RunningApp/Components/ProfileOptions.js
@@ -1,44 +1,90 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { FontAwesome6 } from '@expo/vector-icons';
-import { Ionicons} from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 import { FontAwesome } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
 const styles = StyleSheet.create({
     listItemContainer: {
         width: '80%',
-        backgroundCOlor: 'white',
-        borderRadius: 10,
+        backgroundColor: 'white',
         padding: 20,
-        marginBottom: 10,
-        justifyContent: 'center',
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
         alignItems: 'center',
     },
+    listItemContent: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    listItemTitle: {
+        marginLeft: 10,
+    },
+    chevronIcon: {
+        marginLeft: 'auto',
+    },
 });	
+
+const CustomListItem = ({ title, iconLibrary, iconName, onPress }) => {
+    return (
+      <TouchableOpacity onPress={onPress} style={styles.listItemContainer}>
+        <View style={styles.listItemContent}>
+          {iconLibrary === "FontAwesome6" && (
+            <FontAwesome6 name={iconName} size={24} color="black" />
+          )}
+          {iconLibrary === "Ionicons" && (
+            <Ionicons name={iconName} size={24} color="gray" />
+          )}
+          {iconLibrary === "FontAwesome" && (
+            <FontAwesome name={iconName} size={24} color="yellow" />
+          )}
+          <Text style={styles.listItemTitle}>{title}</Text>
+        </View>
+        <Ionicons name="chevron-forward" size={24} color="gray" style={styles.chevronIcon} />
+      </TouchableOpacity>
+    );
+};
+
 const ProfileOptions = () => {
+    const navigation = useNavigation();
+
     const optionsData = [
-        { title: 'My Community', iconLibrary: 'FontAwesome6', iconName:"people-group", onPress: () => console.log('My Community') },
-        { title: 'Run Records', iconLibrary: 'FontAwesome6', iconName:"person-running", onPress: () => console.log('Run Records') },
-        { title: 'Settings', iconLibrary: 'Ionicons', iconName:"settings-sharp", onPress: () => console.log('Settings') },
-        { title: 'Ratings', iconLibrary: 'FontAwesome', iconName:"star", onPress: () => console.log('Ratings') },
+        { 
+            title: 'My Community', 
+            iconLibrary: 'FontAwesome6', 
+            iconName: 'people-group', onPress: () => navigation.navigate('Community') 
+        },
+        { 
+            title: 'Run Records', 
+            iconLibrary: 'FontAwesome6', 
+            iconName: 'person-running', onPress: () => navigation.navigate('History') 
+        },
+        { 
+            title: 'Settings', 
+            iconLibrary: 'Ionicons', 
+            iconName: 'settings-sharp', onPress: () => console.log('Settings') 
+        },
+        { 
+            title: 'Ratings', 
+            iconLibrary: 'FontAwesome', 
+            iconName: 'star', onPress: () => console.log('Ratings') 
+        },
     ];
 
     return (
         <View>
-            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+            <View style={{ justifyContent: 'center', alignItems: 'center', width: '100%' }}>
                 {optionsData.map((item, index) => (
-                    <ListItem key={index} bottomDivider onPress={item.onPress} containerStyle={styles.listItemContainer}>
-                        <ListItem.Content>
-                            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                {item.iconLibrary === 'FontAwesome6' && <FontAwesome6 name={item.iconName} size={24} color="black" />}    
-                                {item.iconLibrary === 'Ionicons' && <Ionicons name={item.iconName} size={24} color="gray" />} 
-                                {item.iconLibrary === 'FontAwesome' && <FontAwesome name={item.iconName} size={24} color="yellow" />}                       
-                                <ListItem.Title style={{ marginLeft: 10 }}>{item.title}</ListItem.Title>
-                            </View>
-                        </ListItem.Content>
-                        <ListItem.Chevron />
-                    </ListItem>
+                    <CustomListItem
+                        key={index}
+                        title={item.title}
+                        iconLibrary={item.iconLibrary}
+                        iconName={item.iconName}
+                        onPress={item.onPress}
+                    />
                 ))}
             </View>
         </View>
